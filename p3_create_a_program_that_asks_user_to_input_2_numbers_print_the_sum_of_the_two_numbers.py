029ed5918f36f63d69c11a2934d286d4e4fafbd9
nashalexisdolores <alxsdlrs@gmail.com>
2026-03-19 18:40:54 +0800

    final commit

---
+++ p3_create_a_program_that_asks_user_to_input_2_numbers_print_the_sum_of_the_two_numbers.py
@@ -0,0 +1,7 @@
+# INPUT NUM
+first_number = float(input("enter first number: "))
+second_number = float(input("enter second number: "))
+# SUM
+
+print(f"sum:", first_number + second_number)
+    
---
+++ p4_create_a_program_that_asks_user_to_input_2_numbers_print_the_product_of_the_two_numbers.py
@@ -0,0 +1,6 @@
+# 
+first_number = float(input("enter first number: "))
+second_number = float(input("enter second number: "))
+# product
+
+print(f"the product is:", first_number * second_number)
